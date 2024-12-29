12d1943fbba7cb4ce8a326ebecd5894a39fd7a0d
madargani <mad3913@gmail.com>
2024-12-28 21:48:44 -0800

    2024-12-28 21:48:44

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 cd $(dirname $0)
+hugo
+git add .
+git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
+git push
 git subtree split --prefix public -b gh-pages-deploy
 git push origin gh-pages-deploy:gh-pages --force
 git branch -D gh-pages-deploy
