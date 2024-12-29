972310eb1ec9d0ef9227bc43d758e4bf5ea0c0f4
madargani <mad3913@gmail.com>
2024-12-28 21:50:57 -0800

    added deploy bash script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 cd $(dirname $0)
+
+# Build Site
 hugo
+
+# Push Main Branch
 git add .
 git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
 git push
+
+# Push Github Pages Branch
 git subtree split --prefix public -b gh-pages-deploy
 git push origin gh-pages-deploy:gh-pages --force
 git branch -D gh-pages-deploy
